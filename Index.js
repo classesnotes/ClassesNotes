55ef3a4f759c6cb46a4aa2f734f54a95ada593d8
classesnotes <classesnotes30@gmail.com>
2024-02-17 07:47:05 +0530

    yes

--- Index.js
+++ Index.js
@@ -1,8 +1,14 @@
 const http = require('node:http');
-const hostname = '0.0.0.0';
+const hostname = '127.0.0.1';
 var fs = require('fs');
 var index = fs.readFileSync('index.js');
 const port = 3000;
+const express = require('express');
+const app = express();
+app.use((req,res,next) => {
+  console.log('first express');
+
+});
 
 
 const server = http.createServer(() => {
